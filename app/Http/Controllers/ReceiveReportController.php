ab2ce9e0870cca4c535f20eb5c0911c19ca636fa
ZubaerShimul <zubaer.cse12@gmail.com>
2024-06-27 22:01:37 +0600

    receive report

--- app/Http/Controllers/ReceiveReportController.php
+++ app/Http/Controllers/ReceiveReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BankAccount;
+use App\Models\PaymentMethod;
 use App\Models\Receive;
 use App\Models\Site;
 use Carbon\Carbon;
@@ -12,15 +13,26 @@ class ReceiveReportController extends Controller
 {
     public function index(Request $request)
     {
-        $to_date = !empty($request->to_date) ? $request->to_date : Carbon::now()->toDateString();
-        $from_date = !empty($request->from_date) ? $request->from_date : null;
+        $to_date = !empty($request->to_date) ? Carbon::parse($request->to_date)->toDateString() : Carbon::now()->toDateString();
+        $from_date = !empty($request->from_date) ? Carbon::parse($request->from_date)->toDateString() : null;
 
         $data['from_date'] = $from_date;
         $data['to_date'] = $to_date;
         $data['site_id'] = $request->site_id;
         $data['account_id'] = $request->account_id;
-        $data['sites'] = Site::whereHas('receive')->orderBy('id', 'desc')->get();
-        $data['accounts'] = BankAccount::whereHas('receive')->orderBy('name', 'asc')->get();
+        $data['site_bank_name'] = $request->site_bank_name;
+        $data['district'] = $request->district;
+        $data['area'] = $request->area;
+        $data['payment_method'] = $request->payment_method;
+
+
+        $data['sites']          = Site::whereHas('receive')->orderBy('name', 'asc')->get();
+        $data['districts']      = Receive::where('district', '!=', '')->get()->unique('district');
+        $data['areas']          = Receive::where('area', '!=', null)->get()->unique('area');
+        $data['accounts']       = BankAccount::orderBy('name', 'asc')->get();
+        // $data['accounts']       = Payment::where('site_bank_name','!=', null)->distinct()->select('site_bank_name')->get();
+        $data['payment_methods'] = PaymentMethod::orderBy('name', 'asc')->get();
+
 
         if (!empty($from_date)) {
             $query = Receive::whereDate('date', '>=', $request->from_date)->whereDate('date', '<=', $to_date);
@@ -34,6 +46,16 @@ class ReceiveReportController extends Controller
         if ($request->has('account_id') && !empty($request->account_id)) {
             $query->where(['account_id' => $request->account_id]);
         }
+        if ($request->has('district') && !empty($request->district)) {
+            $query->where(['district' => $request->district]);
+        }
+        if ($request->has('area') && !empty($request->area)) {
+            $query->where(['area' => $request->area]);
+        }
+        if ($request->has('payment_method') && !empty($request->payment_method)) {
+            $query->where(['payment_method' => $request->payment_method]);
+        }
+
         $receives = $query->get();
 
         return view('report.receive.report', ['data' => $data, 'receives' => $receives]);
